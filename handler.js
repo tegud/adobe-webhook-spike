34e4f903a47c5ea3e7cbc2eb1c3eaa482a5cab14
Steve Elliott <steve.elliott@spareroom.co.uk>
2018-12-04 08:23:02 +0000

    challenge & webhook

--- handler.js
+++ handler.js
@@ -1,7 +1,17 @@
-module.exports.hello = (event, context, callback) => {
+module.exports.challenge = (event, context, callback) => {
   const response = {
     statusCode: 200,
     body: event.queryStringParameters.challenge,
+  };
+
+  console.log(event.body);
+
+  callback(null, response);
+};
+
+module.exports.webhook = (event, context, callback) => {
+  const response = {
+    statusCode: 200
   };
 
   console.log(event.body);
